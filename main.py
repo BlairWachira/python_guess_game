3aad76eda1571edbc7ca23a5778a528f8df76917
BlairWachira <blairwachira@proton.me>
2026-01-19 12:38:09 +0300

    modified main file

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-from database_fun import*
-from game_logic import*
+from database_fun import *
+from game_logic import *
 
-print("do you have a username select below")
-print("select 1 if you have")
-print("select 2 if you do not have")
-d_ans=int(input("enter your answer: ")) 
+def ask_username():
+    while True:
+        print("Do you have a username? Select below:")
+        print("1. Yes")
+        print("2. No")
+        try:
+            d_ans = int(input("Enter your answer (1 or 2): "))
+            if d_ans in [1, 2]:
+                return d_ans
+        except ValueError:
+            pass
+        print("Invalid choice. Try again.\n")
 
-if d_ans==1:
-    username=input("enter your user name: ")
-    if not player_exists(username):
-      print("user not found")
-      exit()
-else:
-  username=input("create a username: ")
-  add_player(username,0)
-  if player_exists(username):
-    print("username already taken")
-    exit()
+def get_username():
+    while True:
+        choice = ask_username()
+        if choice == 1:
+            username = input("Enter your username: ")
+            if player_exists(username):
+                return username
+            else:
+                print("User not found. Try again.\n")
+        else:  
+            username = input("Create a new username: ")
+            if player_exists(username):
+                print("Username already taken. Try again.\n")
+            else:
+                add_player(username, 0)
+                return username
 
-
-score=0
+username = get_username()
+score = 0
 for i in range(10):
-  score += logic_game()
+    score += logic_game()
 
-update_score(username,score)
-print(f"Game over! {username}, your score is {score}/10")
+update_score(username, score)
+print(f"\nGame over! {username}, your score is {score}/10")
 
-print("\n LEADERBOARD ")
+print("\nLEADERBOARD")
 for rank, (user, s) in enumerate(get_leaderboard(), start=1):
     print(f"{rank}. {user} → {s}")
+
 
 
